6f395523ee9946d08741a7f2cf772ac04ab16440
Joaquin <joacovillar93@gmail.com>
2018-06-14 00:35:03 -0300

    start optimizer

--- lab2018/Optimizer.hs
+++ lab2018/Optimizer.hs
@@ -10,10 +10,71 @@
 module Optimizer where
 
 import Syntax
+import Debug.Trace
 -- se pueden agregar mas importaciones
 -- en caso de ser necesario
 
-
+debug = flip trace
 
 optimize :: Program -> Program
-optimize = undefined -- Implementar 
+optimize (Program name defs body) = (Program name defs (reduceIt body))
+
+reduceIt :: Body -> Body
+reduceIt [] = []
+reduceIt ((Assig name expr):xs) = [Assig name (reduce expr)] ++ reduceIt xs
+reduceIt ((If expr body bodyD):xs) = [If (reduce expr) (reduceIt body) (reduceIt bodyD)] ++ reduceIt xs
+reduceIt ((While expr body):xs) = [While (reduce expr) (reduceIt body)] ++ reduceIt xs
+reduceIt ((Write expr):xs) = [Write (reduce expr)] ++ reduceIt xs
+reduceIt ((Read name):xs) = [Read name] ++ reduceIt xs
+
+
+reduce :: Expr -> Expr
+reduce (IntLit integer) = IntLit integer
+reduce (BoolLit bool) = BoolLit bool
+reduce (Binary Or (BoolLit x) (BoolLit y)) = if x == False && y == False then BoolLit False else BoolLit True
+reduce (Binary Or (BoolLit True) _) = BoolLit True
+reduce (Binary Or _ (BoolLit True)) = BoolLit True
+reduce (Binary Or (BoolLit False) expr) = reduceBasic(Binary Or (BoolLit False) (reduce expr)) --cambiar
+reduce (Binary Or expr (BoolLit False)) = reduceBasic(Binary Or (reduce expr) (BoolLit False)) --cambiar
+reduce (Binary And (BoolLit x) (BoolLit y)) = if x == True && y == True then BoolLit True else BoolLit False
+reduce (Binary And (BoolLit False) _) = BoolLit False
+reduce (Binary And _ (BoolLit False)) = BoolLit False
+reduce (Binary And expr (BoolLit True)) = reduceBasic(Binary And (reduce expr) (BoolLit True)) --cambiar
+reduce (Binary And (BoolLit True) expr) = reduceBasic(Binary And (BoolLit True) (reduce expr)) --cambiar
+reduce (Unary Not (BoolLit True)) = BoolLit False
+reduce (Unary Not (BoolLit False)) = BoolLit True
+reduce (Unary Not expr) =  reduceBasic(Unary Not (reduce expr)) --cambiar 
+reduce (Binary Equ (IntLit x) (IntLit y)) = if x == y then BoolLit True else BoolLit False
+reduce (Binary Equ exp expd) = reduceBasic(Binary Equ (reduce exp) (reduce expd)) --cambiar
+reduce (Binary Less (IntLit x) (IntLit y)) = if x < y then BoolLit True else BoolLit False
+reduce (Binary Less exp expd) = reduceBasic(Binary Less (reduce exp) (reduce expd)) --cambiar
+reduce (Binary Plus (IntLit x) (IntLit y)) = IntLit (x + y)
+reduce (Binary Plus exp expd) = reduceBasic(Binary Plus (reduce exp) (reduce expd)) --cambiar
+reduce (Binary Minus (IntLit x) (IntLit y)) = IntLit (x - y)
+reduce (Binary Minus exp expd) = reduceBasic(Binary Minus (reduce exp) (reduce expd)) --cambiar
+reduce (Binary Mult (IntLit x) (IntLit y)) = IntLit (x * y)
+reduce (Binary Mult exp expd) = reduceBasic(Binary Mult (reduce exp) (reduce expd)) --cambiar
+reduce (Binary Div (IntLit x) (IntLit y)) = IntLit (div x y)
+reduce (Binary Div exp expd) = reduceBasic(Binary Div (reduce exp) (reduce expd)) --cambiar
+reduce (Binary Mod (IntLit x) (IntLit y)) = IntLit (mod x y)
+reduce (Binary Mod exp expd) = reduceBasic(Binary Mod (reduce exp) (reduce expd)) --cambiar
+
+reduceBasic :: Expr -> Expr
+reduceBasic (IntLit integer) = IntLit integer
+reduceBasic (BoolLit bool) = BoolLit bool
+reduceBasic (Binary Or (BoolLit x) (BoolLit y)) = if x == False && y == False then BoolLit False else BoolLit True
+reduceBasic (Binary Or (BoolLit True) _) = BoolLit True
+reduceBasic (Binary Or _ (BoolLit True)) = BoolLit True
+reduceBasic (Binary And (BoolLit x) (BoolLit y)) = if x == True && y == True then BoolLit True else BoolLit False
+reduceBasic (Binary And (BoolLit False) _) = BoolLit False
+reduceBasic (Binary And _ (BoolLit False)) = BoolLit False
+reduceBasic (Unary Not (BoolLit True)) = BoolLit False
+reduceBasic (Unary Not (BoolLit False)) = BoolLit True
+reduceBasic (Binary Equ (IntLit x) (IntLit y)) = if x == y then BoolLit True else BoolLit False
+reduceBasic (Binary Less (IntLit x) (IntLit y)) = if x < y then BoolLit True else BoolLit False
+reduceBasic (Binary Plus (IntLit x) (IntLit y)) = IntLit (x + y)
+reduceBasic (Binary Minus (IntLit x) (IntLit y)) = IntLit (x - y)
+reduceBasic (Binary Mult (IntLit x) (IntLit y)) = IntLit (x * y)
+reduceBasic (Binary Div (IntLit x) (IntLit y)) = IntLit (div x y)
+reduceBasic (Binary Mod (IntLit x) (IntLit y)) = IntLit (mod x y)
+reduceBasic x = x 
